1015fc0b0b8886d47fcf14cea1d3acb60cb60597
becooq81 <77732679+becooq81@users.noreply.github.com>
2023-04-09 23:52:19 +0900

    Add files via upload

---
+++ src/main/java/net/cn/ComputerNetwork/ComputerNetworkApplication.java
@@ -0,0 +1,13 @@
+package net.cn.ComputerNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerNetworkApplication.class, args);
+	}
+
+}
